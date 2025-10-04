69011a1a7c859a12c2d83f44c46201a029782711
Nguyen Xuan Toan <96753996+Neeeap@users.noreply.github.com>
2025-10-04 13:01:58 +0700

    first version of card

--- jav.js
+++ jav.js
@@ -1,5 +1,5 @@
 let books=[]
-let bookMake= function(name,author,pages,read){
+function BookMake(name,author,pages,read){
     this.name=name
     this,author=author
     this.pages=pages
@@ -7,8 +7,29 @@ let bookMake= function(name,author,pages,read){
     this.read=read
 }
 function addLibro(name,author,pages,read){
-    let adder= new bookMake(name,author,pages,read)
+    let adder= new BookMake(name,author,pages,read)
+    display(name,author,pages,read)
     books.push(adder)
+}
+let display=function(name,author,pages,read){
+    let tab=document.createElement("div")
+    tab.style.display="flex"
+    tab.style.width= "200px"
+    tab.style.height="200px"
+    tab.style.flexDirection="column"
+    tab.style.alignContent="center"
+    tab.style.textAlign="center"
+    let text1=document.createElement("p")
+    let text2=document.createElement("p")
+    let text3=document.createElement("p")
+    text3.textContent=`Pages: ${pages}`
+    text1.textContent=name
+    text2.textContent=author
+    let block=document.querySelector("#display")
+    tab.append(text1)
+    tab.append(text2)
+    tab.append(text3)
+    block.append(tab)
 }
 addLibro("harry","jk",100,"yep")
 console.log(books)
