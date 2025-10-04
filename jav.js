e85c2db0d427b78a4a0498a73e594aac35dcb8f7
Nguyen Xuan Toan <96753996+Neeeap@users.noreply.github.com>
2025-10-04 13:24:04 +0700

    added read checkbox and remove button

--- jav.js
+++ jav.js
@@ -13,6 +13,7 @@ function addLibro(name,author,pages,read){
 }
 let display=function(name,author,pages,read){
     let tab=document.createElement("div")
+    let check=document.createElement("div")
     tab.style.display="flex"
     tab.style.width= "200px"
     tab.style.height="200px"
@@ -22,14 +23,30 @@ let display=function(name,author,pages,read){
     let text1=document.createElement("p")
     let text2=document.createElement("p")
     let text3=document.createElement("p")
+    let remove=document.createElement("button")
+    let readCheck=document.createElement("input")
+    let readLabel=document.createElement("label")
+    readCheck.type="checkbox"
+    remove.addEventListener("click",() =>{
+        tab.remove()
+    })
+    readCheck.id=`${name}`
+    readLabel.htmlFor=`${name}`
+    readLabel.textContent="Read"
+    remove.textContent="Remove"
     text3.textContent=`Pages: ${pages}`
     text1.textContent=name
     text2.textContent=author
     let block=document.querySelector("#display")
+    check.append(readCheck)
+    check.append(readLabel)
     tab.append(text1)
     tab.append(text2)
     tab.append(text3)
+    tab.append(check)
+    tab.append(remove)
     block.append(tab)
 }
 addLibro("harry","jk",100,"yep")
+addLibro("jaws","cool man",1000,"nope")
 console.log(books)
