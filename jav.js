de42c7204de0a4e1e75506bec47b1a7db72ad31a
Nguyen Xuan Toan <96753996+Neeeap@users.noreply.github.com>
2025-10-05 14:42:31 +0700

    made not very functional form

--- jav.js
+++ jav.js
@@ -17,12 +17,6 @@ function addLibro(name,author,pages,read){
 let display=function(name,author,pages,read,ref,id){
     let tab=document.createElement("div")
     let check=document.createElement("div")
-    tab.style.display="flex"
-    tab.style.width= "200px"
-    tab.style.height="200px"
-    tab.style.flexDirection="column"
-    tab.style.alignContent="center"
-    tab.style.textAlign="center"
     let text1=document.createElement("p")
     let text2=document.createElement("p")
     let text3=document.createElement("p")
@@ -30,6 +24,16 @@ let display=function(name,author,pages,read,ref,id){
     let readCheck=document.createElement("input")
     let readLabel=document.createElement("label")
     readCheck.type="checkbox"
+    tab.style.display="flex"
+    tab.style.width= "200px"
+    tab.style.height="200px"
+    tab.style.flexDirection="column"
+    tab.style.alignContent="center"
+    tab.style.textAlign="center"
+    tab.style.backgroundColor="beige"
+    tab.style.borderRadius="20px"
+    remove.style.width="100px"
+    remove.style.alignSelf="center"
     remove.addEventListener("click",() =>{
         tab.remove()
         books=books.filter(item => item.id !== ref.id)
@@ -82,4 +86,17 @@ newBook.addEventListener("click", () =>{
     addLibro(name,author,pages,read)
     console.log(books)
 })
+let buttDiag=document.querySelector("#diag")
+let diag=document.querySelector("dialog")
+buttDiag.addEventListener("click", () =>{
+    diag.showModal()
+})
+addLibro("sadfdsfad","DFSdsafdsaf",11,true)
+addLibro("sadfdsfad","DFSdsafdsaf",11,true)
+addLibro("sadfdsfad","DFSdsafdsaf",11,true)
+addLibro("sadfdsfad","DFSdsafdsaf",11,true)
+addLibro("sadfdsfad","DFSdsafdsaf",11,true)
+addLibro("sadfdsfad","DFSdsafdsaf",11,true)
+addLibro("sadfdsfad","DFSdsafdsaf",11,true)
+addLibro("sadfdsfad","DFSdsafdsaf",11,true)
 console.log(books)
